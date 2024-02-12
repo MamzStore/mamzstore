035a944cadd771a3dcd1fb677596b0cdb5d4e742
MamzStore <159632897+MamzStore@users.noreply.github.com>
2024-02-12 17:21:08 +0700

    Add files via upload

--- masuk.js
+++ masuk.js
@@ -8,9 +8,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     window.location.href = 'index.html';
   }
 if (username === 'imam' && password === '12345') {
-    window.location.href = 'pgwniawpptiqbaoqdxwl.html';
+    window.location.href = 'imam/pgwniawpptiqbaoqdxwl.html';
   }
- if (username === 'imamsaputra' && password === '12345') {
-    window.location.href = 'pgwniawpptiqbaoqdxwl.html';
+ if (username === 'GOAT_MD' && password === '112233') {
+    window.location.href = 'abbasid/veffmmcuqy.html';
   } 
 });
